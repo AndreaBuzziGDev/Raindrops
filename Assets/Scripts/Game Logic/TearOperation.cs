f8fc244bd6fd43ff01e56a42900692ec0a0f1b2b
Andrea Buzzi <aby.development@outlook.it>
2024-03-18 00:58:34 +0100

    Updated basic script

--- Assets/Scripts/Game Logic/TearOperation.cs
+++ Assets/Scripts/Game Logic/TearOperation.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TearOperation : MonoBehaviour
 {
+    //ENUMS
+    public enum EOperation
+    {
+        SUM,
+        DIFFERENCE,
+        MULTIPLICATION,
+        DIVISION
+    }
+
+
+    //DATA
+    [SerializeField] private float tearSpeed = 1.0f;
+    [SerializeField] private EOperation operation = EOperation.SUM;//TODO: THIS DOES NOT NEED TO BE A SERIALIZED FIELD AND SHOULD NOT BE.
+
+
+
+    //LIFECYCLE FUNCTIONS
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //SET POSITION TO STANDARD POSITION
+        //TODO: MOVE TO APPROPRIATE POSITION
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = transform.position + new Vector3(0, tearSpeed * Time.fixedDeltaTime, 0);
     }
+
+
+
+
+    //FUNCTIONALITIES
+
+
+
 }
