9946fc1584fdbc14bc6aaeb69a8103a9b4e051ca
Andrea Buzzi <aby.development@outlook.it>
2024-03-17 22:29:29 +0100

    Created Stage Data Scriptable Object

---
+++ Assets/Scripts/Main Controllers/StageDataSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Stage Data", menuName = "Stage Data")]
+public class StageDataSO : ScriptableObject
+{
+    #region DATA
+
+    //ASSOCIATED SCENE
+    [SerializeField] private string associatedSceneName;
+    public string AssociatedSceneName { get { return associatedSceneName; } }
+    
+    //ENUM IDENTIFIER
+    [SerializeField] private SceneNavigationController.eSceneName stageID;
+    public SceneNavigationController.eSceneName StageID { get { return stageID; } }
+    
+    #endregion
+
+
+
+}
