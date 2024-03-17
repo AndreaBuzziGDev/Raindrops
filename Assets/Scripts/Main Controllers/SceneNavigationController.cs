5a749a007e9eedae9901254ee6f1f8db91d39bc9
Andrea Buzzi <aby.development@outlook.it>
2024-03-17 22:29:53 +0100

    WIP implementing scene navigatio controller, meanwhile creating necessary support objects

--- Assets/Scripts/Main Controllers/SceneNavigationController.cs
+++ Assets/Scripts/Main Controllers/SceneNavigationController.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneNavigationController : MonoBehaviour
 {
+    public enum eSceneName
+    {
+        TitleScreen,
+        MainMenu,
+        Credits,
+        RaindropsGame
+    }
+    
+    
+    #region LIFECYCLE FUNCTIONS
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +27,16 @@ public class SceneNavigationController : MonoBehaviour
     {
         
     }
+    
+    #endregion
+
+    #region FUNCTIONALITIES
+    public void LoadScene(eSceneName targetScene)
+    {
+        
+        
+    }
+    
+    #endregion
+
 }
