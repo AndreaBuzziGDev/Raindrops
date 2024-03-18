457f6e0485f2b9667ae6f947808fa58336e50794
Andrea Buzzi <aby.development@outlook.it>
2024-03-19 00:31:28 +0100

    Got rid of unused Start function

--- Assets/Scripts/Game Logic/TearOperationSpawner.cs
+++ Assets/Scripts/Game Logic/TearOperationSpawner.cs
@@ -13,21 +13,7 @@ public class TearOperationSpawner : MonoSingleton<TearOperationSpawner>
 
 
     //LIFECYCLE FUNCTIONS
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector3 v1 = Vector3.Lerp(leftEnd, rightEnd, 0);
-        Vector3 v2 = Vector3.Lerp(leftEnd, rightEnd, 0.25f);
-        Vector3 v3 = Vector3.Lerp(leftEnd, rightEnd, 0.5f);
-        Vector3 v4 = Vector3.Lerp(leftEnd, rightEnd, 0.75f);
-        Vector3 v5 = Vector3.Lerp(leftEnd, rightEnd, 1);
-        Debug.Log("Vector 1: " + v1);
-        Debug.Log("Vector 2: " + v2);
-        Debug.Log("Vector 3: " + v3);
-        Debug.Log("Vector 4: " + v4);
-        Debug.Log("Vector 5: " + v5);
-    }
+    //...
 
     //GIZMO DRAWING
     void OnDrawGizmos()
