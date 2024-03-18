7d2e90c0a0d433a4e5f4ec4f37824a9cddd9801f
Andrea Buzzi <aby.development@outlook.it>
2024-03-19 00:49:42 +0100

    Implemented collision detection and added some TODOs

--- Assets/Scripts/Game Logic/TearOperationLowerBoundary.cs
+++ Assets/Scripts/Game Logic/TearOperationLowerBoundary.cs
@@ -7,8 +7,14 @@ public class TearOperationLowerBoundary : MonoBehaviour
     
     void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log("Collision");
-        
+        TearOperation tear = coll.gameObject?.GetComponent<TearOperation>();
+        if(tear != null)
+        {
+            Debug.Log("Collision");
+            //TODO: DESTROY TEAR
+            //TODO: EVENTUALLY HANDLE THE REST OF THE GAME LOGIC
+            //TODO: SHOULD REALLY CAST AN EVENT AND LET SOMETHING ELSE HANDLE IT
+        }
     }
 
 }
