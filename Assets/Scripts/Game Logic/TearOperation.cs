48c2bc555a5c91fde61e96782453c04d1d82fd55
Andrea Buzzi <aby.development@outlook.it>
2024-03-18 17:51:35 +0100

    Adding Result calculation to start

--- Assets/Scripts/Game Logic/TearOperation.cs
+++ Assets/Scripts/Game Logic/TearOperation.cs
@@ -58,7 +58,7 @@ public class TearOperation : MonoBehaviour
         
         //SETS THE VISUAL CONTENT OF THE PREFAB
         SetContent();
-        
+        CalcResult();
     }
 
     // Update is called once per frame
