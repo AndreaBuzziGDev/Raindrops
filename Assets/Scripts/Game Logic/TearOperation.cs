9a04345adfadfcb6348e60d69c399a4eb0a86c42
Andrea Buzzi <aby.development@outlook.it>
2024-03-19 20:07:59 +0100

    Modified so that it uses down vector

--- Assets/Scripts/Game Logic/TearOperation.cs
+++ Assets/Scripts/Game Logic/TearOperation.cs
@@ -66,7 +66,7 @@ public class TearOperation : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = transform.position + new Vector3(0, -(tearSpeed * Time.fixedDeltaTime), 0);
+        transform.position = transform.position + Vector3.down * tearSpeed * Time.fixedDeltaTime;
     }
 
 
