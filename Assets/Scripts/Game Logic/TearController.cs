7f90bd0bac05fbcd35bf47ef7d4fb248a94ee41d
Andrea Buzzi <aby.development@outlook.it>
2024-03-19 15:15:57 +0100

    Set up basic script structure

--- Assets/Scripts/Game Logic/TearController.cs
+++ Assets/Scripts/Game Logic/TearController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TearController : MonoSingleton<TearController>
 {
+    //DATA
+    
+
+
+    //LIFECYCLE FUNCTIONS
+
     // Start is called before the first frame update
     void Start()
     {
+        //TODO: THIS SHOULD INITIALIZE A NUMBER OF TEAR OPERATIONS (AND POOL IT AT A LATER STAGE)
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //TODO: THIS SHOULD HANDLE POOLING OF OBJECTS
         
     }
+    
+    
+    
+    //UTILITIES
+    
+    
 }
