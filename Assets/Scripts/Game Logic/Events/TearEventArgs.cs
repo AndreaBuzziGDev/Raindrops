b1b44f824a826338cfdcafc8213736e763bc72c0
Andrea Buzzi <aby.development@outlook.it>
2024-03-22 03:57:03 +0100

    Updated event constructor

--- Assets/Scripts/Game Logic/Events/TearEventArgs.cs
+++ Assets/Scripts/Game Logic/Events/TearEventArgs.cs
@@ -5,14 +5,30 @@ using System;
 
 public class TearEventArgs : EventArgs
 {
+    //ENUMS
+    public enum EType
+    {
+        LOSS,
+        SUCCESS
+    }
+
+
     //DATA
     private TearOperation lostTear;
     public TearOperation LostTear { get { return lostTear; } }
 
+
+    private EType eventType = EType.LOSS;
+    public EType EventType { get { return eventType; } }
+
+
+
+
     //CONSTRUCTOR
-    public TearEventArgs (TearOperation tear)
+    public TearEventArgs (TearOperation tear, EType eventType = EType.LOSS)
     {
         this.lostTear = tear;
+        this.eventType = eventType;
     }
 
 }
