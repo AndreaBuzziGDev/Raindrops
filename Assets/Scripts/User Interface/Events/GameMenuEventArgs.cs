bf42e40a309bd074eb76e0b59530d1fc4d7ca538
Andrea Buzzi <aby.development@outlook.it>
2024-03-31 01:34:21 +0100

    Events for UI, created Game Menu Events

---
+++ Assets/Scripts/User Interface/Events/GameMenuEventArgs.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameMenuEventArgs : EventArgs
+{
+    //ENUMS
+    public enum EType
+    {
+        GAME_MENU_PAUSE_OPEN,
+        GAME_MENU_PAUSE_CLOSE
+    }
+
+    //DATA
+    private EType eventType;
+    public EType EventType { get { return eventType; } }
+
+
+
+    //CONSTRUCTOR
+    public GameMenuEventArgs (EType eventType = EType.GAME_MENU_PAUSE_OPEN)
+    {
+        this.eventType = eventType;
+    }
+}
