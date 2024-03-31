ed60cae55a30838fb0a3c8fabc65a3e03c3c098a
Andrea Buzzi <aby.development@outlook.it>
2024-04-01 01:24:23 +0200

    Implemented Credits. Expecting rework for asset section

---
+++ Assets/Scripts/User Interface/UI_Credits.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UI_Credits : MonoBehaviour
+{
+    //DATA
+    ///INPUT - EVENT-DRIVEN IMPLEMENTATION
+    private RaindropsAction inputPlayer = null;
+    public RaindropsAction InputPlayer { get { return inputPlayer; } }
+
+
+
+    //GAMEOBJECT REFERENCES
+    [SerializeField] SceneNavigationController snc;
+
+
+    //LIFECYCLE FUNCTIONS
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ENABLE INPUT WHEN OBJECT ENABLED
+        inputPlayer = new RaindropsAction();
+        inputPlayer.Enable();
+
+        //ACTION SUBSCRIPTIONS
+        //ESCAPE
+        inputPlayer.BaseActionMap.Escape.performed += OnEscapePerformed;
+    }
+
+
+
+
+    //FUNCTIONALITIES
+    public void HandleBack() => snc.LoadScene(SceneNavigationController.eSceneName.MainMenu);
+
+
+
+
+    //INPUT EVENTS
+    //EVENT-BASED INPUT IMPLEMENTATION
+    private void OnEscapePerformed(InputAction.CallbackContext value)
+    {
+        snc.LoadScene(SceneNavigationController.eSceneName.MainMenu);
+    }
+
+}
