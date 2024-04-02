389e8accb0ee0df76145600d10989efce289abfc
Andrea Buzzi <aby.development@outlook.it>
2024-04-03 01:27:22 +0200

    Added a new value

--- Assets/Scripts/Save Data/SaveController.cs
+++ Assets/Scripts/Save Data/SaveController.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public static class SaveController
 {
     //DATA
+    //PATHS
     public readonly static string defaultGameStatsName = "GameStats";
+    public readonly static string gameSpeed = "GameSpeed";
 }
