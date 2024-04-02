bf99edf342a1e9bf6b1ed4299a83187e21b82d7b
Andrea Buzzi <aby.development@outlook.it>
2024-04-02 02:18:23 +0200

    Implementing switch code for UI Game Over Panel

--- Assets/Scripts/User Interface/UI_GameOverPanel.cs
+++ Assets/Scripts/User Interface/UI_GameOverPanel.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class UI_GameOverPanel : MonoBehaviour
 {
+    //DATA
+
+
+    //GAMEOBJECT REFERENCES
+    [SerializeField] CanvasRenderer gameOverCanvas;//TODO: USE TO ADDRESS VISIBILITY
+
+
+    //LIFECYCLE FUNCTIONS
     // Start is called before the first frame update
     void Start()
     {
+        //DISABLE CANVAS
+        gameOverCanvas.gameObject.SetActive(false);
         
+        //LISTEN EVENTS
+        UI_RaindropsGame.GameMenuEA += HandleGameMenuEvent;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        //UN-LISTEN EVENTS
+        UI_RaindropsGame.GameMenuEA -= HandleGameMenuEvent;
+    }
+
+
+
+    //FUNCTIONALITIES
+    ///GUI BUTTONS
+    //TODO: IMPLEMENT
+    
+
+    //EVENT HANDLING
+    public void HandleGameMenuEvent(object sender, GameMenuEventArgs e)
+    {
+        switch(e.EventType)
+        {
+            case GameMenuEventArgs.EType.GAME_MENU_PAUSE_OPEN:
+            case GameMenuEventArgs.EType.GAME_MENU_PAUSE_CLOSE:
+                break;
+            case GameMenuEventArgs.EType.GAME_OVER:
+                gameOverCanvas.gameObject.SetActive(true);
+                break;
+        }
     }
 }
