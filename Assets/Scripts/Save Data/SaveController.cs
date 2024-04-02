17e2ffd8f5942877af9a20addc304bc277129f36
Andrea Buzzi <aby.development@outlook.it>
2024-04-02 17:51:15 +0200

    Added Controller

---
+++ Assets/Scripts/Save Data/SaveController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveController
+{
+    //DATA
+    public readonly static string defaultGameStatsName = "GameStats";
+}
