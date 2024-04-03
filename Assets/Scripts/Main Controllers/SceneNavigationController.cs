a1301ada2de91753a941abbf5097238ef165f5fa
Andrea Buzzi <aby.development@outlook.it>
2024-04-03 16:56:52 +0200

    Made it a Monosingleton Script for ease of usage

--- Assets/Scripts/Main Controllers/SceneNavigationController.cs
+++ Assets/Scripts/Main Controllers/SceneNavigationController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneNavigationController : MonoBehaviour
+public class SceneNavigationController : MonoSingleton<SceneNavigationController>
 {
     //ENUMS
     #region ENUMS
