cbfb0a6166bd2c67ec451e81e32137bae8bd4fa4
Andrea Buzzi <aby.development@outlook.it>
2024-04-03 16:22:34 +0200

    Added Note

--- Assets/Scripts/User Interface/UI_MainOptions.cs
+++ Assets/Scripts/User Interface/UI_MainOptions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NB: THIS SCRIPT (AS WELL AS UI_MainMenu) CAN BE REFACTORED WITH A COMMON PARENT TO ALLOW CONFIGURATION TO BE MUCH MORE ADAPTABLE AND SIMPLIFIED
 public class UI_MainOptions : MonoBehaviour
 {
     //DATA
@@ -9,19 +10,40 @@ public class UI_MainOptions : MonoBehaviour
 
 
     //GAMEOBJECT REFERENCES
-    [SerializeField] SceneNavigationController snc;//TODO: MAKE IT A MONOSINGLETON?
+    [SerializeField] Canvas optionsMenuPanel;
+    
     
 
     //LIFECYCLE FUNCTIONS
     // Start is called before the first frame update
     void Start()
     {
-        
+        //LISTEN EVENTS
+        UI_RaindropsMainMenu.MainMenuEA += HandleMainMenuEvent;
     }
+    void OnDestroy()
+    {
+        //UN-LISTEN EVENTS
+        UI_RaindropsMainMenu.MainMenuEA -= HandleMainMenuEvent;
+    }
+    
 
 
     //FUNCTIONALITIES
     ///GUI BUTTONS
     
-    //...
+
+    //EVENT HANDLING
+    public void HandleMainMenuEvent(object sender, MainMenuEventArgs e)
+    {
+        switch(e.EventType)
+        {
+            case MainMenuEventArgs.EType.MAIN_MENU:
+                optionsMenuPanel.gameObject.SetActive(true);
+                break;
+            default:
+                optionsMenuPanel.gameObject.SetActive(false);
+                break;
+        }
+    }
 }
