de807c5cd4fdfabb950c6a7f452c45e2d295b336
Andrea Buzzi <aby.development@outlook.it>
2024-04-03 16:10:55 +0200

    Implemented the logic for the game's main menu, this will be the driver script for the main menu UI logic

--- Assets/Scripts/User Interface/UI_RaindropsMainMenu.cs
+++ Assets/Scripts/User Interface/UI_RaindropsMainMenu.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
-public class UI_RaindropsMainMenu : MonoBehaviour
+public class UI_RaindropsMainMenu : MonoSingleton<UI_RaindropsMainMenu>
 {
+    //EVENTS
+    public static event EventHandler<MainMenuEventArgs> MainMenuEA;
+
+
+    //DATA
+    ///INPUT - EVENT-DRIVEN IMPLEMENTATION
+    private RaindropsAction inputPlayer = null;
+    public RaindropsAction InputPlayer { get { return inputPlayer; } }
+
+
+
+
+    //PREFAB REFERENCES
+
+
+
+
+
+    //LIFECYCLE FUNCTIONS
     // Start is called before the first frame update
     void Start()
     {
-        
+        //TODO: AT THE START, THE UI IS INITIALIZED TO MAIN MENU VISUALIZATION
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        //ENABLE INPUT WHEN OBJECT STARTS
+        inputPlayer = new RaindropsAction();
+        inputPlayer.Enable();
+
+        //ACTION SUBSCRIPTIONS
+        //ESCAPE
+        inputPlayer.BaseActionMap.Escape.performed += OnEscapePerformed;
     }
+
+
+
+
+    //INPUT EVENTS
+    //EVENT-BASED INPUT IMPLEMENTATION
+    private void OnEscapePerformed(InputAction.CallbackContext value)
+    {
+        if(GameController.Instance.IsPlaying)
+            GameController.Instance.SetState(GameController.EGameState.Paused);
+        else if(!GameController.Instance.IsGameOver)
+            GameController.Instance.SetState(GameController.EGameState.Playing);
+    }
+
+    //FUNCTIONALITIES
+
+
+
+
+    //UTILITIES
+
+
+
+
+    
+    //EVENT-FIRING METHOD
+    public static void OnEscPressed(object sender, MainMenuEventArgs myEventArg) => MainMenuEA?.Invoke(sender, myEventArg);
+
 }
