002248597e5c01d374979fb9a4b4fcac7b62c06b
Andrea Buzzi <aby.development@outlook.it>
2024-04-04 17:15:11 +0200

    Minor comment change

--- Assets/Scripts/Game Logic/DifficultySettingSO.cs
+++ Assets/Scripts/Game Logic/DifficultySettingSO.cs
@@ -8,7 +8,7 @@ public class DifficultySettingSO : ScriptableObject
     //DATA
     [SerializeField] UtilsPrefs.GameSettings.DIFFICULTY difficultyValue = UtilsPrefs.GameSettings.DIFFICULTY.EASY;
 
-    //TODO: FORCE THESE TO BE A POSITIVE VALUE
+    //NB: WOULD BE COOL TO FORCE THESE TO BE A POSITIVE VALUE
     [SerializeField] float speedCoefficient = 1.0f;
     [SerializeField] float scoreCoefficient = 1.0f;
 
