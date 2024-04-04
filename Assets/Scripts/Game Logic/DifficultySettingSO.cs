d879ee943eb3933efd14d70fb46827e45ba2f83e
Andrea Buzzi <aby.development@outlook.it>
2024-04-04 17:11:31 +0200

    Adjusted content and created Difficulty Settings

--- Assets/Scripts/Game Logic/DifficultySettingSO.cs
+++ Assets/Scripts/Game Logic/DifficultySettingSO.cs
@@ -8,13 +8,13 @@ public class DifficultySettingSO : ScriptableObject
     //DATA
     [SerializeField] UtilsPrefs.GameSettings.DIFFICULTY difficultyValue = UtilsPrefs.GameSettings.DIFFICULTY.EASY;
 
-    //TODO: FORCE IT TO BE A POSITIVE VALUE
-    [SerializeField] float scoreCoefficient = 1.0f;
+    //TODO: FORCE THESE TO BE A POSITIVE VALUE
     [SerializeField] float speedCoefficient = 1.0f;
+    [SerializeField] float scoreCoefficient = 1.0f;
 
     //DATA GETTERS
     public UtilsPrefs.GameSettings.DIFFICULTY DifficultyValue { get { return difficultyValue; } }
-    public float ScoreCoefficient { get { return scoreCoefficient; } }
     public float SpeedCoefficient { get { return speedCoefficient; } }
+    public float ScoreCoefficient { get { return scoreCoefficient; } }
 
 }
