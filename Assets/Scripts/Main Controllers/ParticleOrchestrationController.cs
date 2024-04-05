8fa709fe8eb8002ed1373db0f82f7d7093371b85
Andrea Buzzi <aby.development@outlook.it>
2024-04-05 16:54:42 +0200

    Added new controller for handling Particle Effects

---
+++ Assets/Scripts/Main Controllers/ParticleOrchestrationController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOrchestrationController : MonoSingleton<ParticleOrchestrationController>
+{
+    //DATA
+
+
+
+
+    //PREFABS
+
+
+
+    //LIFECYCLE FUNCTIONS
+    // Start is called before the first frame update
+    void Start()
+    {
+        //REGISTER EVENTS
+        
+    }
+
+    //FUNCTIONALITIES
+    
+
+}
